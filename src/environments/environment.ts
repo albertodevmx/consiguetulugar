9ccbe858fa6ebefe11f4066675d6762b5905eb2a
Alberto Hernández Ángel <>
2026-02-18 23:08:50 -0600

    package corregido

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   stripe: {
     publishableKey: 'pk_live_51Sz4SUF6dierXOV1FuvXOpUTySYHJdnfFFMIxj5pFQd2pn2TyC6f0sUoYf5gkVaE6rgrLqpapwMBNeaumzxOH7Il00QOsvW7pI',
     // This is set after creating a Product + Price in Stripe Dashboard
-    priceId: 'REPLACE_WITH_STRIPE_PRICE_ID',
+    priceId: 'price_1T2P5NF6dierXOV14OC12DVk',
   },
   // Cloud Function URL — set after deploying functions
   functionsUrl: 'https://us-central1-estudiarbarato.cloudfunctions.net',
